7fdce9fac71b0c9bd0860ace5dc1e37db9912c0c
anwatson1982 <anwatson1982@gmail.com>
2020-12-30 12:20:25 +0000

    Add project files.

---
+++ Lashes_CRM_WebApp/Data/Customer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+        [Required]
+    public DateTime ProductionDate { get; set; }
+    
+    public string LashType;
+        public List<string> LashSize;
+        public string TreatmentType;
+        public bool ActiveUser = true;
+        private bool _subscriberList = true;
+
+
+        //   [XmlIgnore]
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+
+        //   public bool SubscriberList { get => _subscriberList; set => _subscriberList = value; }
+
+        //   public Customer()
+        //   {
+        //       date = DateTime.Now;
+        //  }
+
+        public override string ToString()
+        {
+            return $"{CustomerID} - {FullName} - {EmailAddress} - {PhoneNumber}";
+        }
+    }
---
+++ Lashes_CRM_WebApp/Pages/Add_User.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lashes_CRM_WebApp.Pages
+{
+    partial class Add_User
+    {
+    }
+}
